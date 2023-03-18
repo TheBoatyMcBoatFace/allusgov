686dca5e67e0b484ce9afc1d1c615d5ba18c73b2
Bentley Hensel <bentley.hensel@civicactions.com>
2023-03-17 20:37:18 -0400

    Update compare_and_upload.py

--- compare_and_upload.py
+++ compare_and_upload.py
@@ -1,20 +1,6 @@
 import os
 import json
-import git
-from git import Repo
 from github import Github
-
-def apply_label(g, repo_name, commit_sha, label_name):
-    repo = g.get_repo(repo_name)
-    labels = [repo.get_label(label_name)]
-    commit = repo.get_commit(sha=commit_sha)
-    commit.create_status(
-        state="success",
-        description=f"Applied label: {label_name}",
-        context=f"Label: {label_name}",
-        target_url="",
-    )
-    commit.set_labels(*labels)
 
 # Compare Data
 def compare_data(data_path, agency_name):
@@ -49,34 +35,13 @@ def compare_data(data_path, agency_name):
     # Calculate the percentage of changes
     percentage_changed = (len(symmetric_difference) / len(union)) * 100
 
-    # Initialize the Github client
-    g = Github(os.environ["GITHUB_TOKEN"])
-    repo_name = os.environ["REPO"]
-    repo = g.get_repo(repo_name)  # Add this line to get the repo object
-
-    # Apply the appropriate labels based on the changes
-    if percentage_changed == 0:
-        label_name = "No Changes"
-    elif percentage_changed <= 10:
-        label_name = "Tiny Changes"
-    elif percentage_changed <= 30:
-        label_name = "Medium Changes"
-    elif percentage_changed <= 50:
-        label_name = "Big Changes"
-    else:
-        label_name = "Major Changes"
-
-    # Apply the label to the current commit
-    commit_sha = repo.get_commits()[0].sha
-    apply_label(g, repo_name, commit_sha, label_name)
-
+    print(f"Percentage of changes: {percentage_changed}%")
+    print(f"Added entries: {len(added_entries)}")
+    print(f"Removed entries: {len(removed_entries)}")
 
 # End Compare Data
-
 
 if __name__ == "__main__":
     agency_name = os.environ["AGENCY_NAME"]
     agency_data_path = os.environ["AGENCY_DATA_PATH"]
     compare_data(agency_data_path, agency_name)
-
-
